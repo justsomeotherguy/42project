ae7a11862fa05567dc5243d0dcc864a6afab3f04
Jason Williamson <jwilliam@F2K023.42adel.org.au>
2021-11-16 15:42:58 +1030

    Add code3 files to repository

---
+++ Code3/code3.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   code3.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwilliam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 15:38:22 by jwilliam          #+#    #+#             */
+/*   Updated: 2021/11/16 15:42:31 by jwilliam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+#include	<stdio.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int		multi;
+	int		result;
+
+	multi = 1;
+	result = 1;
+	if (nb == 0)
+	{
+		return (1);
+	}
+	else if (nb < 0)
+	{
+		return (0);
+	}
+	while (multi <= nb)
+	{
+		result = result * multi;
+		multi++;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_iterative_factorial(8));
+}
